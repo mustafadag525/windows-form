26ff7da71eea92db82813f707cd5c74e4794cc89
mustafadag525 <55752233+mustafadag525@users.noreply.github.com>
2021-02-10 17:00:36 +0300

    Add files via upload

---
+++ OgrenciNotHesaplama/OgrenciNotHesaplama/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace OgrenciNotHesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnhesapla = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtvize = new System.Windows.Forms.TextBox();
+            this.txtfinal = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblortalama = new System.Windows.Forms.Label();
+            this.lbldurum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnhesapla
+            // 
+            this.btnhesapla.Location = new System.Drawing.Point(125, 243);
+            this.btnhesapla.Name = "btnhesapla";
+            this.btnhesapla.Size = new System.Drawing.Size(180, 62);
+            this.btnhesapla.TabIndex = 0;
+            this.btnhesapla.Text = "Hesapla";
+            this.btnhesapla.UseVisualStyleBackColor = true;
+            this.btnhesapla.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "vize";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "final";
+            // 
+            // txtvize
+            // 
+            this.txtvize.Location = new System.Drawing.Point(125, 101);
+            this.txtvize.Name = "txtvize";
+            this.txtvize.Size = new System.Drawing.Size(100, 22);
+            this.txtvize.TabIndex = 3;
+            // 
+            // txtfinal
+            // 
+            this.txtfinal.Location = new System.Drawing.Point(125, 161);
+            this.txtfinal.Name = "txtfinal";
+            this.txtfinal.Size = new System.Drawing.Size(100, 22);
+            this.txtfinal.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(356, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Ortalama";
+            this.label4.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(356, 166);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Durum";
+            // 
+            // lblortalama
+            // 
+            this.lblortalama.AutoSize = true;
+            this.lblortalama.Location = new System.Drawing.Point(465, 88);
+            this.lblortalama.Name = "lblortalama";
+            this.lblortalama.Size = new System.Drawing.Size(46, 17);
+            this.lblortalama.TabIndex = 7;
+            this.lblortalama.Text = "label5";
+            // 
+            // lbldurum
+            // 
+            this.lbldurum.AutoSize = true;
+            this.lbldurum.Location = new System.Drawing.Point(465, 166);
+            this.lbldurum.Name = "lbldurum";
+            this.lbldurum.Size = new System.Drawing.Size(46, 17);
+            this.lbldurum.TabIndex = 8;
+            this.lbldurum.Text = "label6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 448);
+            this.Controls.Add(this.lbldurum);
+            this.Controls.Add(this.lblortalama);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtfinal);
+            this.Controls.Add(this.txtvize);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnhesapla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnhesapla;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtvize;
+        private System.Windows.Forms.TextBox txtfinal;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblortalama;
+        private System.Windows.Forms.Label lbldurum;
+    }
+}
+
---
+++ OgrenciNotHesaplama/OgrenciNotHesaplama/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OgrenciNotHesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double vize, final, ortalama;
+            vize = Convert.ToDouble(txtvize.Text);
+            final = Convert.ToDouble(txtfinal.Text);
+            ortalama = vize * 0.4 + final * 0.6;
+            lblortalama.Text = ortalama.ToString();
+            if(ortalama<60)
+            {
+                lbldurum.Text = "kaldınız";
+                this.BackColor = Color.Red;
+            }
+            else
+            {
+                lbldurum.Text = "geçtiniz";
+                this.BackColor = Color.Green;
+            }
+        }
+    }
+}

c13f6ba29f49c093cac8ae145946b65133c7cd87
mustafadag525 <55752233+mustafadag525@users.noreply.github.com>
2021-02-13 00:03:20 +0300

    Add files via upload

---
+++ kullanıcı cevap verdiğinde ekranı temizleme.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mesaageboxapp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btntemizle_Click(object sender, EventArgs e)
+        {
+            DialogResult res= MessageBox.Show("temizlemek istediğinizden emin misiniz ?", "mesaj", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if(res==DialogResult.Yes)
+            {
+                //temizle();
+               
+            }
+            else
+            {
+                
+            }
+        }
+        
+
+        //private void temizle()       [[1. yol]]
+        //{
+        //    foreach (Control item in this.Controls) form ekranını evet denildiğinde silmek için
+        //    {
+        //        if(item is TextBox)
+        //        {
+        //            TextBox T = (TextBox)item;
+        //            T.Text = string.Empty;
+        //        }
+
+        //    }
+        //}
+
+
+        //private void temizle2()  2. yol
+        //{
+        //    ((TextBox)this.Controls["txtisim"]).Text = string.Empty;
+        //}
+
+
+    }
+}
+

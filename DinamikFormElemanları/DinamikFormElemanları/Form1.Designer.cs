6a64822d862a400ac933b8084075af06737aecb9
mustafadag525 <55752233+mustafadag525@users.noreply.github.com>
2021-02-14 02:50:56 +0300

    Add files via upload

---
+++ DinamikFormElemanları/DinamikFormElemanları/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace DinamikFormElemanları
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnuret = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // btnuret
+            // 
+            this.btnuret.Location = new System.Drawing.Point(631, 12);
+            this.btnuret.Name = "btnuret";
+            this.btnuret.Size = new System.Drawing.Size(108, 113);
+            this.btnuret.TabIndex = 0;
+            this.btnuret.Text = "Üret";
+            this.btnuret.UseVisualStyleBackColor = true;
+            this.btnuret.Click += new System.EventHandler(this.btnuret_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(615, 254);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 283);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.btnuret);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnuret;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ DinamikFormElemanları/DinamikFormElemanları/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DinamikFormElemanları
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnuret_Click(object sender, EventArgs e)
+        {
+            for(int i=1;i<=50;i++)
+            {
+                Button btntemp = new Button();
+                btntemp.Name = "btn" + i.ToString();
+                btntemp.Size= new System.Drawing.Size(35, 35);
+                btntemp.Text = i.ToString();
+                btntemp.UseVisualStyleBackColor = true;
+                flowLayoutPanel1.Controls.Add(btntemp);
+
+            }
+        }
+    }
+}
+ //this.btntemp.Location = new System.Drawing.Point(3, 3);
+ //           this.btntemp.Name = "btntemp";
+ //           this.btntemp.Size = new System.Drawing.Size(35, 35);
+ //           this.btntemp.TabIndex = 2;
+ //           this.btntemp.Text = "1";
+ //           this.btntemp.UseVisualStyleBackColor = true;

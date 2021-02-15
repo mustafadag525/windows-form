481f2a1cb61f6ac1fe1939e25d0ac9eb85a27079
mustafadag525 <55752233+mustafadag525@users.noreply.github.com>
2021-02-15 22:36:18 +0300

    Add files via upload

---
+++ MayınTarlasıOyunu/MayınTarlasıOyunu/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace MayınTarlasıOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnuret = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.lblskor = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblmayın = new System.Windows.Forms.Label();
+            this.lbl = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnuret
+            // 
+            this.btnuret.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.btnuret.Location = new System.Drawing.Point(623, 12);
+            this.btnuret.Name = "btnuret";
+            this.btnuret.Size = new System.Drawing.Size(110, 110);
+            this.btnuret.TabIndex = 0;
+            this.btnuret.Text = "Üret";
+            this.btnuret.UseVisualStyleBackColor = false;
+            this.btnuret.Click += new System.EventHandler(this.btnuret_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(600, 266);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // lblskor
+            // 
+            this.lblskor.AutoSize = true;
+            this.lblskor.Location = new System.Drawing.Point(620, 172);
+            this.lblskor.Name = "lblskor";
+            this.lblskor.Size = new System.Drawing.Size(47, 17);
+            this.lblskor.TabIndex = 2;
+            this.lblskor.Text = "SKOR";
+            this.lblskor.Click += new System.EventHandler(this.lblskor_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(620, 209);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "MAYIN";
+            this.label1.Click += new System.EventHandler(this.lblskor_Click);
+            // 
+            // lblmayın
+            // 
+            this.lblmayın.AutoSize = true;
+            this.lblmayın.Location = new System.Drawing.Point(687, 209);
+            this.lblmayın.Name = "lblmayın";
+            this.lblmayın.Size = new System.Drawing.Size(16, 17);
+            this.lblmayın.TabIndex = 3;
+            this.lblmayın.Text = "0";
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(687, 172);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(0, 17);
+            this.lbl.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(687, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 282);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.lblmayın);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblskor);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.btnuret);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnuret;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label lblskor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblmayın;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MayınTarlasıOyunu/MayınTarlasıOyunu/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MayınTarlasıOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lblskor.Text = "0";
+        }
+
+        private void btnuret_Click(object sender, EventArgs e)
+        {
+            int mayın1 = 0;
+           int mayın2 = 0;
+           int  mayın3 = 0;
+
+            Random rnd = new Random();
+           mayın1= rnd.Next(1, 20);
+           mayın2= rnd.Next(21, 40);
+           mayın3= rnd.Next(41, 50);
+
+
+            for(int i=1;i<=50;i++)
+            {
+                Button btntemp = new Button();
+                btntemp.Name = "btn" + i.ToString();
+                btntemp.Size = new System.Drawing.Size(35, 35);
+                btntemp.Text = i.ToString();
+                btntemp.UseVisualStyleBackColor = true;
+                if(mayın1==i || mayın2==i || mayın3==i)
+                {
+                    btntemp.Tag = true;
+                }
+                else
+                {
+                    btntemp.Tag = false;
+                }
+
+                
+                btntemp.Click += Btntemp_Click;
+                flowLayoutPanel1.Controls.Add(btntemp);
+
+            }
+
+        }
+        private void Btntemp_Click(object sender, EventArgs e)
+        {
+            Button basılan = ((Button)sender);
+            bool mayınbulundumu =(bool)basılan.Tag;
+
+            if(mayınbulundumu)
+            {
+                MessageBox.Show("Maalesef mayına bastınız");
+                basılan.BackColor = Color.Red;
+                int skorınt = int.Parse(lblmayın.Text);
+                skorınt++;
+                lblmayın.Text = skorınt.ToString();
+            }
+            else
+            {
+                basılan.BackColor = Color.Green;
+                int skorınt = int.Parse(lblskor.Text);
+                skorınt++;
+                lblskor.Text = skorınt.ToString();
+            }
+        }
+
+        private void lblskor_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+//this.btntemp.Location = new System.Drawing.Point(3, 3);
+//            this.btntemp.Name = "btntemp";
+//            this.btntemp.Size = new System.Drawing.Size(35, 35);
+//            this.btntemp.TabIndex = 0;
+//            this.btntemp.Text = "1";
+//            this.btntemp.UseVisualStyleBackColor = true;
